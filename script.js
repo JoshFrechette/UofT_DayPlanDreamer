dceddc4977453b4cd34c642ed622c27bcad5f9b6
Josh Frechette <josh.f.design@gmail.com>
2019-10-24 23:57:34 -0400

    Initial commit with file set up

---
+++ script.js
@@ -0,0 +1,6 @@
+//Global variables
+now = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(now)
+
+document.getElementById("currentDay").innerHTML = now ;
+//Display current date and time in the jumbotron section
