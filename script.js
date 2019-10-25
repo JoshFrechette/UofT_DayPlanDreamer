dc827f3c570671c6edeb62e2511bc71b7a4c0667
Josh Frechette <josh.f.design@gmail.com>
2019-10-25 14:30:25 -0400

    Ambitious attempt to have html elements created as DOMJavascript. Going to reel it back in and attempt this way at a better time. Reverting back to each time field being classic html blocks of code.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //Global variables
-now = moment().format('MMMM Do YYYY, h:mm:ss a');
+//Time Variables
+var rightNow = moment().format('h'); 
+console.log(rightNow)
+var now = moment().format('MMMM Do YYYY'); // Declare a variable 
 console.log(now)
 
 document.getElementById("currentDay").innerHTML = now ;
-//Display current date and time in the jumbotron section
+//Display current date and hour in the jumbotron section
+var agendaColor = 0; //Colour of bar responds to past, present, or future styling 
+var i =0;
+
+var agendaInput = document.querySelector("agendaInput");
+
+var agendaHourly = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+
+$(document).ready(function() {
+//var hourly = agendaHourly[i++];
+for (i = 0; i < agendaHourly.length; i++){
+document.getElementById("hour").innerHTML = agendaHourly;
+}
+
+});
